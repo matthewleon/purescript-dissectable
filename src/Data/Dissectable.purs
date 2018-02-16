be1fa5660d6c49967c0cacce4b2b3f9ecff86a1b
Matthew Leon <ml@matthewleon.com>
2018-02-16 15:18:54 -0500

    moveLeft

--- src/Data/Dissectable.purs
+++ src/Data/Dissectable.purs
@@ -19,21 +19,28 @@ import Partial.Unsafe (unsafePartial)
 -- Largely cribbed from Phil Freeman's "Stack-Safe Traversals via Dissection"
 -- http://blog.functorial.com/posts/2017-06-18-Stack-Safe-Traversals-via-Dissection.html
 
--- TODO: moveLeft
 class (Traversable f, Bifunctor d) <= Dissectable f d | f -> d where
   moveRight :: forall c j. Either (f j) (Tuple (d c j) c) -> Either (f c) (Tuple (d c j) j)
+  moveLeft  :: forall c j. Either (f c) (Tuple (d c j) j) -> Either (f j) (Tuple (d c j) c)
 
--- TODO: compare implementation with lazy list
--- This might gain speed by not actually reversing the list when that's unnecessary
 instance dissectableArray :: Dissectable Array (Product (Clown List) (Joker List)) where
-  moveRight (Left xs) =
-    case A.head xs of
+  moveRight (Left js) =
+    case A.head js of
       Nothing -> Left []
-      Just x  -> Right (Tuple (Product (Clown Nil) (Joker $ unsafePartial $ fromJust $ tail $ L.fromFoldable xs)) x)
+      Just j  -> Right (Tuple (Product (Clown Nil) (Joker $ unsafePartial $ fromJust $ tail $ L.fromFoldable js)) j)
   moveRight (Right (Tuple (Product (Clown cs) (Joker js)) c)) =
     case js of
       Nil -> Left $ A.reverse $ A.fromFoldable $ c : cs
       j : js' -> Right (Tuple (Product (Clown (c : cs)) (Joker js')) j)
+
+  moveLeft (Left cs) =
+    case A.head cs of
+      Nothing -> Left []
+      Just c  -> Right (Tuple (Product (Clown $ unsafePartial $ fromJust $ tail $ L.fromFoldable cs) (Joker Nil)) c)
+  moveLeft (Right (Tuple (Product (Clown cs) (Joker js)) j)) =
+    case cs of
+      Nil -> Left $ A.reverse $ A.fromFoldable $ j : js
+      c : cs' -> Right (Tuple (Product (Clown cs') (Joker (j : js))) c)
 
 instance dissectableList :: Dissectable List (Product (Clown List) (Joker List)) where
   moveRight (Left Nil) = Left Nil
@@ -42,6 +49,13 @@ instance dissectableList :: Dissectable List (Product (Clown List) (Joker List))
     case js of
       Nil -> Left (reverse (c : cs))
       j : js' -> Right (Tuple (Product (Clown (c : cs)) (Joker js')) j)
+
+  moveLeft (Left Nil) = Left Nil
+  moveLeft (Left (c : cs)) = Right (Tuple (Product (Clown cs) (Joker Nil)) c)
+  moveLeft (Right (Tuple (Product (Clown cs) (Joker js)) j)) =
+    case cs of
+      Nil -> Left (reverse (j : js))
+      c : cs' -> Right (Tuple (Product (Clown cs') (Joker (j : js))) c)
 
 mapP :: forall f d a b. Dissectable f d => (a -> b) -> f a -> f b
 mapP f xs = tailRec step (Left xs) where
