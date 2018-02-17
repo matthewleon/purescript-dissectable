8abc04cbf393d17f0f2d23c834883f71e96ae8d9
Matthew Leon <ml@matthewleon.com>
2018-02-16 21:43:02 -0500

    kill instances

--- src/Data/Dissectable.purs
+++ src/Data/Dissectable.purs
@@ -3,14 +3,8 @@ module Data.Dissectable where
 import Prelude
 
 import Control.Monad.Rec.Class (class MonadRec, Step(..), tailRec, tailRecM)
-import Data.Array as A
 import Data.Bifunctor (class Bifunctor)
-import Data.Bifunctor.Clown (Clown(..))
-import Data.Bifunctor.Joker (Joker(..))
-import Data.Bifunctor.Product (Product(..))
 import Data.Either (Either(..))
-import Data.List (List(..), (:))
-import Data.List as L
 import Data.Maybe (Maybe(..))
 import Data.Monoid (class Monoid, mempty)
 import Data.Traversable (class Traversable)
@@ -24,58 +18,9 @@ class (Traversable f, Bifunctor d) <= Dissectable f d | f -> d where
   moveRight :: forall c j. Either (f j) (Tuple (d c j) c) -> Either (f c) (Tuple (d c j) j)
   moveLeft  :: forall c j. Either (f c) (Tuple (d c j) j) -> Either (f j) (Tuple (d c j) c)
 
--- | The `Dissectable` instance of `Array` uses `Lists` to represent its zipper.
--- | A more canonical instance would use `Arrays` for this purpose, at the cost
--- | of O(n^2) traversal.
-instance dissectableArray :: Dissectable Array (Product (Clown List) (Joker List)) where
-  moveRight (Left js) =
-    case L.uncons $ L.fromFoldable js of
-      Nothing                   -> Left []
-      Just {head: j, tail: js'} -> Right $ Tuple
-        (Product
-          (Clown Nil)
-          (Joker js'))
-        j
-  moveRight (Right (Tuple (Product (Clown cs) (Joker js)) c)) =
-    case js of
-      Nil     -> Left  $ A.reverse $ A.fromFoldable $ c : cs
-      j : js' -> Right $ Tuple
-        (Product
-          (Clown $ c : cs)
-          (Joker js'))
-        j
-
-  moveLeft (Left cs) =
-    case L.uncons $ L.fromFoldable cs of
-      Nothing                   -> Left []
-      Just {head: c, tail: cs'} -> Right $ Tuple
-        (Product
-          (Clown cs')
-          (Joker Nil))
-        c
-  moveLeft (Right (Tuple (Product (Clown cs) (Joker js)) j)) =
-    case cs of
-      Nil     -> Left  $ A.reverse $ A.fromFoldable $ j : js
-      c : cs' -> Right $ Tuple
-        (Product
-          (Clown cs')
-          (Joker (j : js)))
-        c
-
-instance dissectableList :: Dissectable List (Product (Clown List) (Joker List)) where
-  moveRight (Left Nil) = Left Nil
-  moveRight (Left (j : js)) = Right (Tuple (Product (Clown Nil) (Joker js)) j)
-  moveRight (Right (Tuple (Product (Clown cs) (Joker js)) c)) =
-    case js of
-      Nil -> Left (L.reverse (c : cs))
-      j : js' -> Right (Tuple (Product (Clown (c : cs)) (Joker js')) j)
-
-  moveLeft (Left Nil) = Left Nil
-  moveLeft (Left (c : cs)) = Right (Tuple (Product (Clown cs) (Joker Nil)) c)
-  moveLeft (Right (Tuple (Product (Clown cs) (Joker js)) j)) =
-    case cs of
-      Nil -> Left (L.reverse (j : js))
-      c : cs' -> Right (Tuple (Product (Clown cs') (Joker (j : js))) c)
+-- instance for Maybe
+-- instance for Either
+-- instance for Tuple?
 
 mapD :: forall f d a b. Dissectable f d => (a -> b) -> f a -> f b
 mapD f = tailRec step <<< Left
