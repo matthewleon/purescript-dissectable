dc68a0da6006bcf4089c096741a9dbaf44841613
Matthew Leon <ml@matthewleon.com>
2018-02-17 15:03:37 -0500

    kill silly functions (they are on a separate branch now)

--- src/Data/Dissectable.purs
+++ src/Data/Dissectable.purs
@@ -12,10 +12,8 @@ import Data.Either (Either(..))
 import Data.List (List(..), (:))
 import Data.List as L
 import Data.Maybe (Maybe(..))
-import Data.Monoid (class Monoid, mempty)
 import Data.Traversable (class Traversable)
 import Data.Tuple (Tuple(..))
-import Data.Unfoldable (class Unfoldable, unfoldr)
 
 -- Largely cribbed from Phil Freeman's "Stack-Safe Traversals via Dissection"
 -- http://blog.functorial.com/posts/2017-06-18-Stack-Safe-Traversals-via-Dissection.html
@@ -83,24 +81,6 @@ mapD f = tailRec step <<< Left
   step = moveRight >>> case _ of
            Left xs -> Done xs
            Right (Tuple dba a) -> Loop (Right (Tuple dba (f a)))
-
-foldD :: forall f d a b. Dissectable f d => (b -> a -> b) -> b -> f a -> b
-foldD f b = tailRec step <<< Tuple b <<< Left
-  where
-  step (Tuple acc dis) = case moveRight dis of
-           Left  _                 -> Done acc
-           r@(Right (Tuple dba a)) -> Loop $ Tuple (f acc a) r
-
-foldMapD :: forall f d a b. Dissectable f d => Monoid b => (a -> b) -> f a -> b
-foldMapD f = foldD (\b a -> b `append` f a) mempty
-
--- | Convert a `Dissectable` to any `Unfoldable` structure.
-toUnfoldable :: forall f g d. Dissectable f d => Unfoldable g => f ~> g
-toUnfoldable = unfoldr step <<< Left
-  where
-  step = moveRight >>> case _ of
-           Left xs                 -> Nothing
-           r@(Right (Tuple dba a)) -> Just $ Tuple a r
 
 traverseRec
   :: forall m f d a b
